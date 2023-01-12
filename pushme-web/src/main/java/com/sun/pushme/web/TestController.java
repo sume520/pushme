c0bd2aa6f02eb750b4da8b92e7bc7a368c516ac9
sunxiao <292904818@qq.com>
2023-01-12 16:16:34 +0800

    添加logback.xml配置文件

--- pushme-web/src/main/java/com/sun/pushme/web/TestController.java
+++ pushme-web/src/main/java/com/sun/pushme/web/TestController.java
@@ -1,12 +1,15 @@
 package com.sun.pushme.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @RequestMapping("/test")
     private String test(){
+        log.error("log: 测试内容");
         return "Hello World!";
     }
 }
